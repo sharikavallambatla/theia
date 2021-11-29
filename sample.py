c3f42093d5dde858cd38ee17d1678dc6f51ed4de
sharikavallambatla <sharikavallambatlapes@gmail.com>
2021-11-29 22:19:52 +0530

    Added new changes

--- sample.py
+++ sample.py
@@ -1,73 +1,73 @@
-import torch
-import matplotlib.pyplot as plt
-import numpy as np 
-import argparse
-import pickle 
-import os
-from torchvision import transforms 
-from build_vocab import Vocabulary
-from model import EncoderCNN, DecoderRNN
-from PIL import Image
-from build_vocab import Vocabulary
+# import torch
+# import matplotlib.pyplot as plt
+# import numpy as np 
+# import argparse
+# import pickle 
+# import os
+# from torchvision import transforms 
+# from build_vocab import Vocabulary
+# from model import EncoderCNN, DecoderRNN
+# from PIL import Image
+# from build_vocab import Vocabulary
 
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def load_image(image_path, transform=None):
-    image = Image.open(image_path).convert('RGB')
-    image = image.resize([224, 224], Image.LANCZOS)
+# def load_image(image_path, transform=None):
+#     image = Image.open(image_path).convert('RGB')
+#     image = image.resize([224, 224], Image.LANCZOS)
     
-    if transform is not None:
-        image = transform(image).unsqueeze(0)
+#     if transform is not None:
+#         image = transform(image).unsqueeze(0)
     
-    return image
+#     return image
 
-def caption(vocab,imagepath):
+# def caption(vocab,imagepath):
     
-    # with open('data/vocab.pkl', 'rb') as f:
-    #     vocab = pickle.load(f)
-    # Image preprocessing
-    transform = transforms.Compose([
-        transforms.ToTensor(), 
-        transforms.Normalize((0.485, 0.456, 0.406), 
-                             (0.229, 0.224, 0.225))])
+#     # with open('data/vocab.pkl', 'rb') as f:
+#     #     vocab = pickle.load(f)
+#     # Image preprocessing
+#     transform = transforms.Compose([
+#         transforms.ToTensor(), 
+#         transforms.Normalize((0.485, 0.456, 0.406), 
+#                              (0.229, 0.224, 0.225))])
     
-    # Load vocabulary wrapper
+#     # Load vocabulary wrapper
     
 
-    # Build models
-    encoder = EncoderCNN(256).eval()  # eval mode (batchnorm uses moving mean/variance)
-    decoder = DecoderRNN(256, 512, len(vocab), 1)
-    encoder = encoder.to(device)
-    decoder = decoder.to(device)
+#     # Build models
+#     encoder = EncoderCNN(256).eval()  # eval mode (batchnorm uses moving mean/variance)
+#     decoder = DecoderRNN(256, 512, len(vocab), 1)
+#     encoder = encoder.to(device)
+#     decoder = decoder.to(device)
 
-    # Load the trained model parameters
-    encoder.load_state_dict(torch.load('models/encoder-5-3000.pkl'))
-    decoder.load_state_dict(torch.load('models/decoder-5-3000.pkl'))
+#     # Load the trained model parameters
+#     encoder.load_state_dict(torch.load('models/encoder-5-3000.pkl'))
+#     decoder.load_state_dict(torch.load('models/decoder-5-3000.pkl'))
 
-    # Prepare an image
-    image = load_image(imagepath, transform)
-    image_tensor = image.to(device)
+#     # Prepare an image
+#     image = load_image(imagepath, transform)
+#     image_tensor = image.to(device)
     
-    # Generate an caption from the image
-    feature = encoder(image_tensor)
-    sampled_ids = decoder.sample(feature)
-    sampled_ids = sampled_ids[0].cpu().numpy()          # (1, max_seq_length) -> (max_seq_length)
+#     # Generate an caption from the image
+#     feature = encoder(image_tensor)
+#     sampled_ids = decoder.sample(feature)
+#     sampled_ids = sampled_ids[0].cpu().numpy()          # (1, max_seq_length) -> (max_seq_length)
     
-    # Convert word_ids to words
-    sampled_caption = []
-    for word_id in sampled_ids:
-        word = vocab.idx2word[word_id]
-        sampled_caption.append(word)
-        if word == '<end>':
-            break
-    sentence = ' '.join(sampled_caption)
+#     # Convert word_ids to words
+#     sampled_caption = []
+#     for word_id in sampled_ids:
+#         word = vocab.idx2word[word_id]
+#         sampled_caption.append(word)
+#         if word == '<end>':
+#             break
+#     sentence = ' '.join(sampled_caption)
     
-    # Print out the image and the generated caption
-    print (sentence)
-    image = Image.open('png/example.png')
-    plt.imshow(np.asarray(image))
-    return (sentence)
+#     # Print out the image and the generated caption
+#     print (sentence)
+#     image = Image.open('png/example.png')
+#     plt.imshow(np.asarray(image))
+#     return (sentence)
 ##if __name__ == '__main__':
 ##    parser = argparse.ArgumentParser()
 ##    parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
